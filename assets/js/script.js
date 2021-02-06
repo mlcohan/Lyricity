5b41dcad29ddb86a9e387f12024e96a6c2c73b7d
Colin Edwards <ColinJhEdwards@gmail.com>
2021-02-06 09:48:33 -0800

    test please ignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,26 @@ var lastFmApiKey = "8c532e6fce66c0c0cae9e4ef54fbf478";
 var albumDisplay = document.querySelector(".songImg");
 // calling album cover api
 function getFmApi(song, artist) {
-  var requestUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=" + lastFmApiKey + "&artist=" + artist + "&track=" + song + "&format=json"
+  var requestUrl =
+    "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=" +
+    lastFmApiKey +
+    "&artist=" +
+    artist +
+    "&track=" +
+    song +
+    "&format=json";
   fetch(requestUrl)
-    .then(function(response){
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
       albumCover = data.track.album.image[3]["#text"];
-      if(albumCover == ""){
+      if (albumCover == "") {
         albumDisplay.src = "./assets/images/mic.jpg";
       } else {
         albumDisplay.src = albumCover;
       }
-    })
+    });
 }
 // calling kanye rest api
 function kanye() {
@@ -26,9 +33,12 @@ function kanye() {
     url: "https://api.kanye.rest",
     method: "GET",
   }).then(function (data) {
-    lyrics.innerHTML = "Sorry, we didn't find any lyrics but here's a nice quote from Kanye!<br> " + '"' + data.quote + '"<br> - Kanye West';
-    albumDisplay.src = "./assets/images/kanye-west.jpg"
-    
+    lyrics.innerHTML =
+      "Sorry, we didn't find any lyrics but here's a nice quote from Kanye!<br> " +
+      '"' +
+      data.quote +
+      '"<br> - Kanye West';
+    albumDisplay.src = "./assets/images/kanye-west.jpg";
   });
 }
 // defining function that calls the server-side API for lyrics
@@ -41,7 +51,7 @@ function getLyricsApi() {
       return response.json();
     })
     .then(function (data) {
-      if(data.lyrics === ""){
+      if (data.lyrics === "") {
         kanye();
       } else {
         lyrics.innerText = data.lyrics;
@@ -61,6 +71,7 @@ $("#closeFavs").on("click", function () {
 // new code for localStorage, stores the song and artist inputs
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
+  $(".main").addClass("fade");
   var button = $(this);
   var song = button.siblings("#song").val();
   var artist = button.siblings("#artist").val();
@@ -76,12 +87,13 @@ searchBtn.addEventListener("click", function (event) {
   displayName(song, artist);
 });
 function displayName(song, artist) {
-  document.getElementById('songNameDisplay').innerText = titleCase(song) + " by " + titleCase(artist);
+  document.getElementById("songNameDisplay").innerText =
+    titleCase(song) + " by " + titleCase(artist);
 }
 function titleCase(string) {
   var sentence = string.toLowerCase().split();
-  for(var i = 0; i< sentence.length; i++){
-     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  for (var i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
   }
-return sentence;
+  return sentence;
 }
